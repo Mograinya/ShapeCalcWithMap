869dedc718b9654f1694ab2793b0609b18ebac25
Mograinya <mograinya@gmail.com>
2022-02-20 22:48:40 +0700

    renaming_complete

--- main.go
+++ main.go
@@ -6,85 +6,82 @@ import (
 	"strings"
 )
 
-type figureStruct struct {
+type shape struct {
 	dimensions         []float64
-	inputRequestPrompt func(fig figureStruct)
-	calcArea           func(fig figureStruct) float64
-	calcPerimeter      func(fig figureStruct) float64
+	inputPromptMessage string
+	inputRequestPrompt func(sh shape)
+	calcArea           func(sh shape) float64
+	calcPerimeter      func(sh shape) float64
 }
 
-var knownFiguresMap = make(map[string]figureStruct)
+var knownShapesMap = make(map[string]shape)
 
-func initFigureMap() {
-	knownFiguresMap["circle"] = figureStruct{
-		dimensions: []float64{0},
-		inputRequestPrompt: func(fig figureStruct) {
-			fmt.Println("Input the radius of a circle:")
-			fmt.Scanf("%f", &fig.dimensions[0])
-		},
-		calcArea:      func(fig figureStruct) float64 { return math.Pi * fig.dimensions[0] * fig.dimensions[0] },
-		calcPerimeter: func(fig figureStruct) float64 { return 2 * math.Pi * fig.dimensions[0] },
+func initShapeMap() {
+	knownShapesMap["circle"] = shape{
+		dimensions:         []float64{0},
+		inputPromptMessage: "Input the radius of a circle:",
+		inputRequestPrompt: func(sh shape) { fmt.Scanf("%f", &sh.dimensions[0]) },
+		calcArea:           func(sh shape) float64 { return math.Pi * sh.dimensions[0] * sh.dimensions[0] },
+		calcPerimeter:      func(sh shape) float64 { return 2 * math.Pi * sh.dimensions[0] },
 	}
 
-	knownFiguresMap["square"] = figureStruct{
-		dimensions: []float64{0},
-		inputRequestPrompt: func(fig figureStruct) {
-			fmt.Println("Input the side length of a square:")
-			fmt.Scanf("%f", &fig.dimensions[0])
-		},
-		calcArea:      func(fig figureStruct) float64 { return fig.dimensions[0] * fig.dimensions[0] },
-		calcPerimeter: func(fig figureStruct) float64 { return 4 * fig.dimensions[0] },
+	knownShapesMap["square"] = shape{
+		dimensions:         []float64{0},
+		inputPromptMessage: "Input the side length of a square:",
+		inputRequestPrompt: func(sh shape) { fmt.Scanf("%f", &sh.dimensions[0]) },
+		calcArea:           func(sh shape) float64 { return sh.dimensions[0] * sh.dimensions[0] },
+		calcPerimeter:      func(sh shape) float64 { return 4 * sh.dimensions[0] },
 	}
 
-	knownFiguresMap["rectangle"] = figureStruct{
-		dimensions: []float64{0, 0},
-		inputRequestPrompt: func(fig figureStruct) {
-			fmt.Println("Input length and width of a rectangle separated by space:")
-			fmt.Scanf("%f %f", &fig.dimensions[0], &fig.dimensions[1])
-		},
-		calcArea:      func(fig figureStruct) float64 { return fig.dimensions[0] * fig.dimensions[1] },
-		calcPerimeter: func(fig figureStruct) float64 { return 2 * (fig.dimensions[0] + fig.dimensions[1]) },
+	knownShapesMap["rectangle"] = shape{
+		dimensions:         []float64{0, 0},
+		inputPromptMessage: "Input length and width of a rectangle (separated by space):",
+		inputRequestPrompt: func(sh shape) { fmt.Scanf("%f %f", &sh.dimensions[0], &sh.dimensions[1]) },
+		calcArea:           func(sh shape) float64 { return sh.dimensions[0] * sh.dimensions[1] },
+		calcPerimeter:      func(sh shape) float64 { return 2 * (sh.dimensions[0] + sh.dimensions[1]) },
 	}
 
-	knownFiguresMap["triangle"] = figureStruct{
-		dimensions: []float64{0, 0, 0},
-		inputRequestPrompt: func(fig figureStruct) {
-			fmt.Println("Input three sides of a tiangle separated by space:")
-			fmt.Scanf("%f %f %f", &fig.dimensions[0], &fig.dimensions[1], &fig.dimensions[2])
+	knownShapesMap["triangle"] = shape{
+		dimensions:         []float64{0, 0, 0},
+		inputPromptMessage: "Input three sides of a triangle (separated by space):",
+		inputRequestPrompt: func(sh shape) {
+			fmt.Scanf("%f %f %f", &sh.dimensions[0], &sh.dimensions[1], &sh.dimensions[2])
 		},
-		calcArea: func(fig figureStruct) float64 {
-			d := fig.dimensions
+		calcArea: func(sh shape) float64 {
+			d := sh.dimensions
 			p := (d[0] + d[1] + d[2]) / 2
 			return math.Sqrt(p * (p - d[0]) * (p - d[1]) * (p - d[2]))
 		},
-		calcPerimeter: func(fig figureStruct) float64 {
-			return fig.dimensions[0] + fig.dimensions[1] + fig.dimensions[2]
+		calcPerimeter: func(sh shape) float64 {
+			return sh.dimensions[0] + sh.dimensions[1] + sh.dimensions[2]
 		},
 	}
 }
 
-func figureTypeRequest() string {
+func requestShapeName() string {
 	var key string
-	fmt.Println("Enter type of figure:")
+	fmt.Println("Enter the name of a shape to calculate:")
 	fmt.Scanf("%s", &key)
 	return strings.ToLower(key)
 }
 
-func printResult(fig figureStruct) {
-	fmt.Printf("\nThe area of this figure is %f", fig.calcArea(fig))
-	fmt.Printf("\nThe perimeter of this figure is %f", fig.calcPerimeter(fig))
+func printResult(sh shape) {
+	fmt.Printf("\nCalculated area: %f", sh.calcArea(sh))
+	fmt.Printf("\nCalculated perimeter: %f", sh.calcPerimeter(sh))
 }
 
 //----------------------------------------------------------------
 
 func main() {
-	initFigureMap()
-	figureType := figureTypeRequest()
-	fig, ok := knownFiguresMap[figureType]
+	initShapeMap()
+	shapeName := requestShapeName()
+	sh, ok := knownShapesMap[shapeName]
 	if !ok {
-		fmt.Println("Unknown figure")
+		fmt.Println("Unknown shape")
 		return
 	}
-	fig.inputRequestPrompt(fig)
-	printResult(fig)
+	fmt.Println(sh.inputPromptMessage)
+	fmt.Scanln() //Без этого следующий Scanf обнаруживает \n от прошлого ввода и выдаёт ошибку
+	sh.inputRequestPrompt(sh)
+	printResult(sh)
 }
